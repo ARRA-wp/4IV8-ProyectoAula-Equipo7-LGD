1128c510ab538e81af9115a55c115ef275a77944
Mar-lopez <mlopezs1904@alumno.ipn.mx>
2021-04-26 21:39:26 -0500

    cascaron

---
+++ JS/Validar.js
@@ -0,0 +1,26 @@
+
+//Validar solo numeros
+function validarn(e){
+    var teclado = (document.all)?e.keyCode:e.which;
+    if(teclado == 8)return true;
+
+    var patron = /[0-9\d .]/;
+
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
+//Validar solo palabras y espcacions
+
+function validarp(e){
+    var teclado = (document.all)?e.keyCode:e.which;
+    if(teclado == 8)return true;
+
+    var patron = /^[a-zA-Z ]*$/;
+
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
+
+
