7c76a0eb35b680d8375d29c19dede098fe1b4d6c
ARRA-wp <72839394+ARRA-wp@users.noreply.github.com>
2021-05-02 10:25:52 -0500

    Validacion

---
+++ JS/Validar.js
@@ -0,0 +1,36 @@
+
+function validare(e) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(valor)){
+     alert("La dirección de email " + valor + " es correcta.");
+    } else {
+     alert("La dirección de email es incorrecta.");
+    }
+  }
+
+
+
+//Validar solo numeros
+function validarn(e){
+    var teclado = (document.all)?e.keyCode:e.which;
+    if(teclado == 8)return true;
+
+    var patron = /[0-9\d .]/;
+
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
+//Validar solo palabras y espcacions
+
+function validarp(e){
+    var teclado = (document.all)?e.keyCode:e.which;
+    if(teclado == 8)return true;
+
+    var patron = /^[a-zA-Z ]*$/;
+
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
+
+
